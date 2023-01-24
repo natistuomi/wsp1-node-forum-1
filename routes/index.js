1552f0f197ad89a9aca905df15322ca08c7366a0
natistuomi <natalie.tuomi@elev.ga.ntig.se>
2023-01-24 12:54:17 +0100

    Del 2 - 4

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/', async function (req, res) {
-    res.send('Hello you!')
+const mysql = require('mysql2');
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
 });
+const promisePool = pool.promise();
+
+router.get('/', async function (req, res, next) {
+    const [rows] = await promisePool.query("SELECT * FROM ja15forum");
+    res.render('index.njk', {
+        rows: rows,
+        title: 'Forum',
+    });
+});
+
+
 
 module.exports = router;
