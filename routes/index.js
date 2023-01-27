358e54e471db4592008e7abb1f6ed887f20a84b9
natistuomi <natalie.tuomi@elev.ga.ntig.se>
2023-01-27 10:55:57 +0100

    scss

--- routes/index.js
+++ routes/index.js
@@ -18,21 +18,17 @@ router.get('/', async function (req, res, next) {
 });
 
 router.post('/new', async function (req, res, next) {
-    const { author, title, content } = req.body;
-
     // Skapa en ny författare om den inte finns men du behöver kontrollera om användare finns!
-    let user = await promisePool.query('SELECT * FROM nt19users WHERE name = ?', [author]);
+    let [user] = await promisePool.query('SELECT * FROM nt19users WHERE id = ?', [authorId]);
     if (!user) {
-        user = await promisePool.query('INSERT INTO nt19users (name) VALUES (?)', [author]);
+        user = await promisePool.query('INSERT INTO nt19users (name) VALUES (?)', [authorName]);
     }
 
     // user.insertId bör innehålla det nya ID:t för författaren
-
     const userId = user.insertId || user[0].id;
 
     // kör frågan för att skapa ett nytt inlägg
     const [rows] = await promisePool.query('INSERT INTO nt19forum (authorId, title, content) VALUES (?, ?, ?)', [userId, title, content]);
-    res.redirect('/'); // den här raden kan vara bra att kommentera ut för felsökning, du kan då använda tex. res.json({rows}) för att se vad som skickas tillbaka från databasen
 });
 
 router.get('/new', async function (req, res, next) {
